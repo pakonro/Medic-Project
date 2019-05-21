2b1f37bffbe110a6cfb02769644e7f6e39fedd7e
Thilo Schuchort <t.schuchort@googlemail.com>
2019-05-21 23:12:29 +0200

    try to create b-scans from m-scan

--- buildImage.m
+++ buildImage.m
@@ -1,19 +1,31 @@
+
+
+
+
 img = mscan;
-%img_subset = img(:, 700000:800000);
-img_subset = img(:, 1:10:1339450);
 
-img_subset_filtered = imgaussfilt(img_subset, 3);
+%img_subset = img(:, 1:10:1339450);
 
-fileID = fopen('oct_timestamp_2.bin');
-A = fread(fileID, 'uint64');
-xTime = 50 / (A(end) - A(1)) / (size(A, 1) ) * 1000 %1 millisekunde in Pixel
-periodLength = xTime * (1/8.75)
-plot(A)
+
+
+fileID = fopen('data/oct_timestamp_2.bin');
+A = fread(fileID, Inf, 'uint64');
+xTime = (A(end) - A(1)) / (size(A, 1)) / 50 / 1000; %seconds per A-scan
+revTime = (1/8.75);
+bscanSize = round(revTime / xTime);
 fclose(fileID);
 
-%figure
+N = 20;
+img_subset = img(:, 600001:(600000 + N*bscanSize));
+img_subset = imgaussfilt(img_subset, 3);
+
+cscan = reshape(img_subset, [512, bscanSize, N]);
+
+%[X Y] = pol2cart( cscan(:,:,2));
+%S = surf(X,Y,ones(size(X))); 
+S = PolarToIm(cscan(:,:,2), 0, 511, 512, 512); 
+figure
 colormap gray
-imagesc(img_subset)
-%figure
-colormap gray
-imagesc(img_subset_filtered)
+%imagesc(img_subset)
+imagesc(S)
+%imagesc(cscan(:,:,8))
