d648ac9686ee67bc04b8a3eb4f88aecf9c496f5e
Thilo Schuchort <t.schuchort@googlemail.com>
2019-09-11 00:18:41 +0200

    Some changes to buildImage.m for taking pictures

--- buildImage.m
+++ buildImage.m
@@ -7,6 +7,9 @@ revTime = (1/8.75);
 bscanSize = round(revTime / xTime);
 fclose(fileID);
 
+%% 
+
+
 figure
 colormap gray
 imagesc(mscan)
@@ -20,6 +23,9 @@ figure
 colormap gray
 imagesc(mscan_subset)
 
+%% 
+
+
 % remove line artifact
 for i = 1:length(mscan_subset)
     col = mscan_subset(215:230, i);
@@ -30,7 +36,10 @@ for i = 1:length(mscan_subset)
 end    
 
 cscan = reshape(mscan_subset, [512, bscanSize, bscan_count]);
+%% 
+
 clearvars mscan_subset
+
 
 cscan_polar = zeros(1024, 1024, size(cscan, 3));
 ellipse_z = zeros(size(cscan, 3), 2);
@@ -40,20 +49,48 @@ ellipse_alpha = zeros(size(cscan, 3));
 
 R = 2;
 
-for i = 1:size(cscan, 3)
+for i = 1%1:size(cscan, 3)
     cscan(:,:,i) = imgaussfilt(cscan(:,:,i), 3);
     bscan_smoothed = imgaussfilt(cscan(150:512,:,i), 8);
     %mscan_edge = int8(edge(mscan_smoothed, "Sobel", 1.2));
     bscan_edge = edge(bscan_smoothed, "Canny", 0.35);
+    [r0, c0] =  find(bscan_edge);
+    
+    figure; 
+    colormap gray;
+    imagesc(bscan_smoothed);
+    hold on;
+    plot(c0, r0, "og");
+    hold off;
+    
     bscan_edge_polar = int8(Polar2Im([zeros(149, bscanSize) ; bscan_edge], 1024, "linear"));
     [r,c] = find(bscan_edge_polar);
+    
     [z, a, b, alpha] = fitellipse([c,r], 'linear', 'constraint', 'trace');
     ellipse_z(i,:) = z;
     ellipse_a(i) = a;
     ellipse_b(i) = b;
     ellipse_alpha(i) = alpha;
     cscan_polar(:,:,i) = Polar2Im(cscan(:,:,i), 1024, "linear");
+    
+    figure; 
+    colormap gray;
+    imagesc(cscan_polar(:,:,i));
+    hold on;
+    plot(c, r, "og");
+    %plotellipse(ellipse_z(i,:), ellipse_a(i), ellipse_b(i), ellipse_alpha(i));
+    hold off;
+    
+    figure; 
+    colormap gray;
+    imagesc(cscan_polar(:,:,i));
+    hold on;
+    plot(c, r, "og");
+    plotellipse(ellipse_z(i,:), ellipse_a(i), ellipse_b(i), ellipse_alpha(i));
+    hold off;
+    
 end
+%% 
 
 clearvars bscan_smoothed bscan_edge bscan_edge_polar
 
